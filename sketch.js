2f4aa9a007c3014438560cc9bcf1f0ebf898f558
daphne1111 <daphane8888@gmail.com>
2024-12-06 10:53:39 +0800

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
   background(220);
   
-  for (let x = -100; x < width; x += 10) {
-    for (let y = -100; y < height; y += 10) {
+  for (let x = -windowWidth; x < windowWidth; x += 10) {
+    for (let y = -windowHeight; y < windowHeight; y += 10) {
       noStroke();
   fill(0,random(60,90));
   square(x+mouseY*0.1,y+mouseX*0.1,9+sin(frameCount*0.01));
