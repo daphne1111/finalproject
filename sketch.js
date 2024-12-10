3b94daf1ca65f680b2ef56560cc9153da631f969
daphne1111 <daphane8888@gmail.com>
2024-12-10 15:06:52 +0800

    Update2 sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,43 @@
-
+let x,y;
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
-  background(220);
-  x=frameCount;
+   background(220);
+  m();
+  
+  /**for (let x = -windowWidth; x < windowWidth; x += 10) {
+    for (let y = -windowHeight; y < windowHeight; y += 10) {
+      noStroke();
+  fill(0,random(60,90));
+  square(x+mouseY*0.5,y+mouseX*0.5,9+sin(frameCount*0.01));
+       if(x>1000){
+    x=0;
+     y=100;
+  }}}**/
+  x=frameCount*2;
   y=sqrt(x);
-  background(220);
-    push();
-  fill(255)
-  rect(100,200,200,10);
+ 
+  push();
+  fill(225)
+  rect(windowWidth/2,windowHeight/2,600,50);
   pop();
+  
+   if(x>3000){
+    x=0;
+     y=100;
+  }
+  push();
+  fill(0);
+  rect(windowWidth/2,windowHeight/2,y,50);
+  pop();
+// console.log(x);
+}
+
+
+function m(){
   for (let x = -windowWidth; x < windowWidth; x += 10) {
     for (let y = -windowHeight; y < windowHeight; y += 10) {
       noStroke();
@@ -20,15 +46,5 @@ function draw() {
        if(x>1000){
     x=0;
      y=100;
-  }
-  push();
-  fill(0);
-  rect(100,200,y,10);
-  pop();
- console.log(x);
-}
-  }}
-
-function loading(){
-  
+  }}}
 }
